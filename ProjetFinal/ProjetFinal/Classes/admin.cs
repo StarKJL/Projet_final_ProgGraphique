92fc76cc64e18dd38f64d5d46387df0ecca530b9
StarKJL <kimhood2007@gmail.com>
2025-12-01 12:42:35 -0500

    d1-1

--- ProjetFinal/ProjetFinal/Classes/admin.cs
+++ ProjetFinal/ProjetFinal/Classes/admin.cs
@@ -11,16 +11,19 @@ namespace ProjetFinal.Classes
         int id;
         string username;
         string password;
+        bool actif;
 
         public admin(int id, string username, string password)
         {
             this.id = id;
             this.username = username;
             this.password = password;
+            this.actif = false;
         }
 
         public int Id { get => id; set => id = value; }
         public string Username { get => username; set => username = value; }
         public string Password { get => password; set => password = value; }
+        public bool Actif { get => actif; set => actif = value; }
     }
 }
