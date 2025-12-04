c8f4bd79ff92698e990cf8b80cc232000fd5bcc3
Star <kimhood2007@gmail.com>
2025-12-04 13:30:05 -0500

    d4
    Ajout de sql fonctions

---
+++ ProjetFinal/ProjetFinalBD/Fonction.sql
@@ -0,0 +1,54 @@
+/*Fonction*/
+
+DELIMITER //
+CREATE FUNCTION calculSalaire(idEmploye VARCHAR(10)) RETURNS INT
+BEGIN
+    DECLARE salaire INT;
+    SELECT nbHrs*tauxHoraire INTO salaire FROM employe INNER JOIN employeprojet e on employe.matricule = e.matricule WHERE matricule=idEmploye;
+ RETURN salaire;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION compteEmployeParProjet(idProjet VARCHAR(12)) RETURNS INT
+BEGIN
+    DECLARE compte INT;
+    SELECT COUNT(*) INTO compte FROM employeprojet WHERE noProjet=idProjet;
+ RETURN compte;
+END//
+DELIMITER ;
+
+
+
+DELIMITER //
+CREATE FUNCTION EmployeImplication(idemploye VARCHAR(10)) RETURNS INT
+BEGIN
+    DECLARE compte INT;
+    SELECT COUNT(*) INTO compte FROM employeprojet WHERE matricule=idemploye;
+ RETURN compte;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION budgetProjet(idProjet VARCHAR(12)) RETURNS DOUBLE
+BEGIN
+    DECLARE nb DOUBLE;
+    SELECT budget INTO nb FROM projet WHERE noProjet=idProjet;
+ RETURN nb;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION totalDesSalaires(idProjet VARCHAR(12)) RETURNS DOUBLE
+BEGIN
+    DECLARE nb double;
+    SELECT totalSalaire INTO nb FROM projet WHERE noProjet=idProjet;
+ RETURN nb;
+END//
+DELIMITER ;
+
+
+
